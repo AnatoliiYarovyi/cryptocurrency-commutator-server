6e656039f5ce4f693f7f6ef3e32af82262733804
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-02-22 13:55:36 +0200

    adding filters by store and time

--- db/queryCoin.js
+++ db/queryCoin.js
@@ -1,17 +1,40 @@
-const queryCoin = async (connection, coin) => {
-  let query = `SELECT cryptocurrensy_symbol AS symbol, AVG(price_average) AS price_one
-FROM cryptocurrency_prices
-WHERE cryptocurrensy_symbol = '${coin}'
-ORDER BY date DESC
-LIMIT 12`;
-  const data = await connection
+const queryCoin = async (
+  connection,
+  coin,
+  shop = 'price_average',
+  limit = 12,
+) => {
+  let query = `SELECT cryptocurrensy_symbol AS symbol, ${shop} AS price 
+  FROM cryptocurrency_prices
+  WHERE cryptocurrensy_symbol = '${coin}'
+  ORDER BY date DESC
+  LIMIT ${limit}`;
+  const dataCoin = await connection
     .promise()
     .query(query)
     .then(([result, field]) => {
-      // console.log(result);
       return result;
     })
     .catch(error => console.error(error));
+
+  // calculate the average value of the price
+  let sumPrice = null;
+  let number = null;
+  const data = dataCoin.reduce((acc, el, i, arr) => {
+    const { symbol, price } = el;
+
+    if (price) {
+      sumPrice += Number(price);
+      ++number;
+    }
+    if (i === arr.length - 1) {
+      acc = {
+        symbol,
+        price: sumPrice / number,
+      };
+    }
+    return acc;
+  }, []);
   return data;
 };
 
