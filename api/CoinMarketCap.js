0f0f95e159669214fa5eee82695dce1ed314dd2d
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-03-15 14:19:04 +0200

    Update CoinMarketCap.js

--- api/CoinMarketCap.js
+++ api/CoinMarketCap.js
@@ -6,27 +6,25 @@ const headers = {
   'X-CMC_PRO_API_KEY': '39ac0feb-d618-47b4-8607-59ea6df21e06',
 };
 
-// -- делаем запрос
 const getArrCoinMarketCap = async () => {
-  return await Promise.all(
-    await axios
-      .get(endPoints, { headers })
-      .then(resp => {
-        const dataCoins = resp.data.data;
-        // console.log(dataCoins);
-        return dataCoins.reduce((acc, el, i) => {
-          acc.push({
-            shop_name: 'coinMarketCap',
-            list_number: i + 1,
-            name: el.name,
-            symbol: el.symbol,
-            price: el.quote.USD.price,
-          });
-          return acc;
-        }, []);
-      })
-      .catch(error => console.log(error)),
-  );
+  const resData = await axios
+    .get(endPoints, { headers })
+    .then(resp => {
+      const dataCoins = resp.data.data;
+      // console.log(dataCoins);
+      return dataCoins.reduce((acc, el, i) => {
+        acc.push({
+          shop_name: 'coinMarketCap',
+          list_number: i + 1,
+          name: el.name,
+          symbol: el.symbol,
+          price: el.quote.USD.price,
+        });
+        return acc;
+      }, []);
+    })
+    .catch(error => console.log(error));
+  return resData;
 };
 
 module.exports = getArrCoinMarketCap;
