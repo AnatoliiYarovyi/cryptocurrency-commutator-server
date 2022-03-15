2f83b6d400ac0e7625a4ac81d8b0d114356d13c2
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-03-15 14:19:26 +0200

    Update Kucoin.js

--- api/Kucoin.js
+++ api/Kucoin.js
@@ -3,33 +3,32 @@ const axios = require('axios');
 const endPoints = 'https://api.kucoin.com/api/v1/prices';
 
 const getArrKucoin = async () => {
-  return await Promise.all(
-    await axios
-      .get(endPoints)
-      .then(resp => {
-        // забираем из ответа обект {} с ключ="имя крипты", значение="стоимость"
-        const objCoins = resp.data.data;
+  const resData = await axios
+    .get(endPoints)
+    .then(resp => {
+      // we take object {} from the response with key="crypt name", value="price"
+      const objCoins = resp.data.data;
 
-        // с помощью "for in" перебираем objCoins и создаем нужный нам масив обектов
-        const dataCoins = [];
-        for (const key in objCoins) {
-          dataCoins.push({
-            symbol: key,
-            price: objCoins[key],
-          });
-        }
+      // using "for in" iterate over objCoins and create the array of objects we need
+      const dataCoins = [];
+      for (const key in objCoins) {
+        dataCoins.push({
+          symbol: key,
+          price: objCoins[key],
+        });
+      }
 
-        return dataCoins.reduce((acc, el, i) => {
-          acc.push({
-            shop_name: 'kucoin',
-            list_number: i + 1,
-            symbol: el.symbol,
-            price: Number(el.price),
-          });
-          return acc;
-        }, []);
-      })
-      .catch(error => console.log(error)),
-  );
+      return dataCoins.reduce((acc, el, i) => {
+        acc.push({
+          shop_name: 'kucoin',
+          list_number: i + 1,
+          symbol: el.symbol,
+          price: Number(el.price),
+        });
+        return acc;
+      }, []);
+    })
+    .catch(error => console.log(error));
+  return resData;
 };
 module.exports = getArrKucoin;
