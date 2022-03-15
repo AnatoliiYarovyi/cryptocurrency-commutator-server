d93d6bd2f2474aeb8f976a9a662133ac760b3f99
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-03-15 14:18:54 +0200

    Update CoinBase.js

--- api/CoinBase.js
+++ api/CoinBase.js
@@ -3,37 +3,36 @@ const axios = require('axios');
 const endPoints = 'https://api.coinbase.com/v2/exchange-rates';
 
 const getCoinBase = async () => {
-  return await Promise.all(
-    await axios
-      .get(endPoints)
-      .then(resp => {
-        // забираем из ответа обект {} с ключ="имя крипты", значение="стоимость"
-        const objCoins = resp.data.data.rates;
+  const resData = await axios
+    .get(endPoints)
+    .then(resp => {
+      // we take object {} from the response with key="crypt name", value="price"
+      const objCoins = resp.data.data.rates;
 
-        // с помощью "for in" перебираем objCoins и создаем нужный нам масив обектов
-        const dataCoins = [];
-        for (const key in objCoins) {
-          dataCoins.push({
-            symbol: key,
-            price: objCoins[key],
-          });
-        }
+      // using "for in" iterate over objCoins and create the array of objects we need
+      const dataCoins = [];
+      for (const key in objCoins) {
+        dataCoins.push({
+          symbol: key,
+          price: objCoins[key],
+        });
+      }
 
-        return dataCoins.reduce((acc, el, i, arr) => {
-          acc.push({
-            shop_name: 'coinBase',
-            list_number: i + 1,
-            symbol: el.symbol,
-            price: Number(el.price),
-          });
-          // if (i === arr.length - 1) {
-          //   console.log(acc);
-          // }
-          return acc;
-        }, []);
-      })
-      .catch(error => console.log(error)),
-  );
+      return dataCoins.reduce((acc, el, i, arr) => {
+        acc.push({
+          shop_name: 'coinBase',
+          list_number: i + 1,
+          symbol: el.symbol,
+          price: Number(el.price),
+        });
+        // if (i === arr.length - 1) {
+        //   console.log(acc);
+        // }
+        return acc;
+      }, []);
+    })
+    .catch(error => console.log(error));
+  return resData;
 };
 // getCoinBase();
 module.exports = getCoinBase;
