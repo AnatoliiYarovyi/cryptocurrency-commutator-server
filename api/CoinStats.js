c98a0fee229695c6db3d5eee095729ad12a59262
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-03-15 14:19:14 +0200

    Update CoinStats.js

--- api/CoinStats.js
+++ api/CoinStats.js
@@ -3,24 +3,23 @@ const axios = require('axios');
 const endPoints = 'https://api.coinstats.app/public/v1/coins?currency=USD';
 
 const getArrCoinStats = async () => {
-  return await Promise.all(
-    await axios
-      .get(endPoints)
-      .then(resp => {
-        const dataCoins = resp.data.coins;
-        return dataCoins.reduce((acc, el, i) => {
-          acc.push({
-            shop_name: 'coinStats',
-            list_number: i + 1,
-            name: el.name,
-            symbol: el.symbol,
-            price: el.price,
-          });
-          return acc;
-        }, []);
-      })
-      .catch(error => console.log(error)),
-  );
+  const resData = await axios
+    .get(endPoints)
+    .then(resp => {
+      const dataCoins = resp.data.coins;
+      return dataCoins.reduce((acc, el, i) => {
+        acc.push({
+          shop_name: 'coinStats',
+          list_number: i + 1,
+          name: el.name,
+          symbol: el.symbol,
+          price: el.price,
+        });
+        return acc;
+      }, []);
+    })
+    .catch(error => console.log(error));
+  return resData;
 };
 
 module.exports = getArrCoinStats;
